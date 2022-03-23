bddf4613547262f41e0c7ed092c298cb82836e52
David Landa <david.landa@protonmail.com>
2022-03-23 11:20:53 +0100

    Update documentation

--- source/ScalarProducts/src/ScalarProducts.jl
+++ source/ScalarProducts/src/ScalarProducts.jl
@@ -5,7 +5,9 @@ using LinearAlgebra
 
 
 """
-    Using the for loop.
+    scalar_product_v1(x, y)
+
+    Compute the scalar product of two vectors using the for loop.
 """
 function scalar_product_v1(x::Array{Float64,1}, y::Array{Float64,1})
     @assert length(x) == length(y)
@@ -21,7 +23,9 @@ end
 
 
 """
-    Using the list comprehension.
+    scalar_product_v2(x, y)
+
+    Compute the scalar product of two vectors using the list comprehension.
 """
 function scalar_product_v2(x::Array{Float64,1}, y::Array{Float64,1})
     return sum([i * j for (i,j) in [x, y]])
@@ -29,7 +33,9 @@ end
 
 
 """
-    Using element-wise `.*` operation.
+    scalar_product_v3(x, y)
+
+    Compute the scalar product of two vectors using element-wise `.*` operation.
 """
 function scalar_product_v3(x::Array{Float64,1}, y::Array{Float64,1})
     return sum(x .* y)
@@ -37,7 +43,9 @@ end
 
 
 """
-    Using builtin `dot` function from `LinearAlgebra` package.
+    scalar_product_v4(x, y)
+
+    Compute the scalar product of two vectors using `LinearAlgebra.dot()` function.
 """
 function scalar_product_v4(x::Array{Float64,1}, y::Array{Float64,1})
     return dot(x, y)
@@ -45,12 +53,19 @@ end
 
 
 """
+    test(F, N)
+
     Generates a couple of random vector of length `N` and returns the time necessary
     for a function `F` from your new module to calculate the result.
 """
 function test(F::Function, N::Int64)
+
     x::Array{Float64, 1} = rand(Float64, N)
     y::Array{Float64, 1} = rand(Float64, N)
+
+    # The function `F` is compiled here and
+    # you like to exclude compile time from test.
+    F(x, y)
 
     return @time F(x, y)
 end
